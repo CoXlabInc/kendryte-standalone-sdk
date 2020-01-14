b3b5d671fec307e94cd94f1f82c89378ef723da8
boguensong <idsonic95@gmail.com>
2020-01-14 14:47:21 +0900

    add rgb888 and jpeg converter

--- src/dvp2sdcard/README.md
+++ src/dvp2sdcard/README.md
@@ -1,6 +1,7 @@
 DVP2SDCARD
 =====
-## Press key to store the 320\*240 rgb565 image from dvp into Micro SD Card.
+## Press key to store the 320\*240 rgb565 or rgb888 image from dvp into Micro SD Card.
+## choose rgb565 or rgb888 by modify main.c
 
 For sipeed m1, press *boot* key.
 
--- src/dvp2sdcard/rgb2bmp.c
+++ src/dvp2sdcard/rgb2bmp.c
@@ -1,5 +1,76 @@
-#include "rgb2bmp.h"
 #include "ff.h"
+#include "rgb2bmp.h"
+int rgb888tobmp(uint8_t *buf,int width,int height, const char *filename)
+{
+	FIL file;
+	FRESULT ret = FR_OK;
+	uint32_t ret_len = 0;
+	uint32_t i;
+	uint16_t temp;
+	uint16_t *ptr;
+
+	BitMapFileHeader bmfHdr;    /* file header */
+	BitMapInfoHeader bmiHdr;    /* information header */
+	// 24bit bitmap don't need pallete
+
+	bmiHdr.biSize = sizeof(BitMapInfoHeader);
+	bmiHdr.biWidth = width;     
+	bmiHdr.biHeight = height;
+	bmiHdr.biPlanes = 1;
+	bmiHdr.biBitCount = 24; //24bit bitmap
+	bmiHdr.biCompression = 0x00; //No compression
+	bmiHdr.biSizeImage = (width * height * 3); //24bit need 3 (8x8x8)
+	bmiHdr.biXPelsPerMeter = 0;
+	bmiHdr.biYPelsPerMeter = 0;
+	bmiHdr.biClrUsed = 0;
+	bmiHdr.biClrImportant = 0;
+
+	bmfHdr.bfType = 0x4D42; //bitmap 'BM' ASCII code
+	// bmfHdr.bfSize = sizeof(BitMapFileHeader) + sizeof(BitMapInfoHeader) + sizeof(RgbQuad)*3 + bmiHdr.biSizeImage;
+	bmfHdr.bfSize = sizeof(BitMapFileHeader) + sizeof(BitMapInfoHeader) + bmiHdr.biSizeImage;
+	bmfHdr.bfReserved1 = 0;
+	bmfHdr.bfReserved2 = 0;
+	// bmfHdr.bfOffBits = sizeof(BitMapFileHeader) + sizeof(BitMapInfoHeader)+ sizeof(RgbQuad)*3;
+	bmfHdr.bfOffBits = sizeof(BitMapFileHeader) + sizeof(BitMapInfoHeader) ;
+
+
+	
+	//rgb565 data to rgb888 data
+	uint8_t *data = malloc( width * height * 3);
+	for (int i = 0, j= 0; i < width* height *3 ; i+= 3, j+=2)
+	{
+		uint16_t buffer = ( buf[j+1] << 8 ) | ( buf[j] ); // ex) [0xF5][0x12]
+		
+		uint8_t r = ((buffer & 0xf800) >> 11 );
+		uint8_t g = ((buffer & 0x7E0) >> 5);
+		uint8_t b = (buffer & 0x1F);
+		
+		//RGB888 - amplify
+  		r <<= 3;
+  		g <<= 2;
+  		b <<= 3;
+		
+		//   printf("data[i] : %x\r\n", (b << 16) | (g << 8) | r );
+		data[i] = b;
+		data[i+1] = g;
+		data[i+2] = r;
+	}
+
+    if ((ret = f_open(&file, filename, FA_CREATE_ALWAYS | FA_WRITE)) != FR_OK) 
+    {
+        printf("create file %s err[%d]\n", filename, ret);
+        return ret;
+    }
+
+    f_write(&file, &bmfHdr, sizeof(BitMapFileHeader), &ret_len);
+    f_write(&file, &bmiHdr, sizeof(BitMapInfoHeader), &ret_len);
+	f_write(&file, data, bmiHdr.biSizeImage, &ret_len);
+	f_close(&file);
+	free(data);
+
+ 
+	return 0;
+}
 
 int rgb565tobmp(uint8_t *buf,int width,int height, const char *filename)
 {
@@ -12,7 +83,7 @@ int rgb565tobmp(uint8_t *buf,int width,int height, const char *filename)
     
 	BitMapFileHeader bmfHdr;    /* file header */
 	BitMapInfoHeader bmiHdr;    /* information header */
-	RgbQuad bmiClr[3];          /* color palette */
+	RgbQuad bmiClr[3]; /*color pallete*/
  
 	bmiHdr.biSize = sizeof(BitMapInfoHeader);
 	bmiHdr.biWidth = width;     
@@ -26,7 +97,9 @@ int rgb565tobmp(uint8_t *buf,int width,int height, const char *filename)
 	bmiHdr.biClrUsed = 0;
 	bmiHdr.biClrImportant = 0;
  
+
 	/* rgb565 mask */
+
 	bmiClr[0].rgbBlue = 0;
 	bmiClr[0].rgbGreen = 0xF8;
 	bmiClr[0].rgbRed = 0;
@@ -42,11 +115,14 @@ int rgb565tobmp(uint8_t *buf,int width,int height, const char *filename)
 	bmiClr[2].rgbRed = 0;
 	bmiClr[2].rgbReserved = 0;
  
-	bmfHdr.bfType = 0x4D42;
-	bmfHdr.bfSize = sizeof(BitMapFileHeader) + sizeof(BitMapInfoHeader) + sizeof(RgbQuad) * 3 + bmiHdr.biSizeImage;
+
+
+
+	bmfHdr.bfType = 0x4D42; //bitmap 'BM' ASCII code
+	bmfHdr.bfSize = sizeof(BitMapFileHeader) + sizeof(BitMapInfoHeader) + sizeof(RgbQuad)*3 + bmiHdr.biSizeImage;
 	bmfHdr.bfReserved1 = 0;
 	bmfHdr.bfReserved2 = 0;
-	bmfHdr.bfOffBits = sizeof(BitMapFileHeader) + sizeof(BitMapInfoHeader)+ sizeof(RgbQuad) * 3;
+	bmfHdr.bfOffBits = sizeof(BitMapFileHeader) + sizeof(BitMapInfoHeader)+ sizeof(RgbQuad)*3;
 
     ptr = (uint16_t*)buf;
     
@@ -65,12 +141,12 @@ int rgb565tobmp(uint8_t *buf,int width,int height, const char *filename)
 
     f_write(&file, &bmfHdr, sizeof(BitMapFileHeader), &ret_len);
     f_write(&file, &bmiHdr, sizeof(BitMapInfoHeader), &ret_len);
-    f_write(&file, &bmiClr, 3 * sizeof(RgbQuad), &ret_len);
-
-    f_write(&file, buf, bmiHdr.biSizeImage, &ret_len);
-
+    f_write(&file, &bmiClr, 3*sizeof(RgbQuad), &ret_len);
+    
+	f_write(&file, buf, bmiHdr.biSizeImage, &ret_len);
 	f_close(&file);
+
  
-	 return 0;
+	return 0;
 }
 
